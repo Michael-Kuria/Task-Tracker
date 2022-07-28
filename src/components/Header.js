d4b746267fc86c55e9ff89b1c7970d3e3bf17a34
micleah <mchege78@gmail.com>
2022-07-28 04:01:19 +0300

    Added Task component

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import Button from './Button';
+
+const Header = (props) => {
+    return (
+        <header className="header">
+            <h1>{props.title}</h1>
+            <Button color ="green" text = "Hello"/>
+        </header>
+    )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+
+export default Header;
